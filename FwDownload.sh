3be1aebe2e7518f36f2e5523686004a23cf07b1b
Rodolfo Turco <informa@turcotronics.it>
2025-05-25 13:59:07 +0200

    Delete FwDownload.sh

--- FwDownload.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# init
-esptool --chip auto --port /dev/ttyUSB0 --baud 460800 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 4MB 0x0 ./CDROM_motors.ino.merged.bin
-read -p "Press any key to continue"
